909a92f2462caa10c2e8e9388a9612de3086b975
zuvega <zvega@my.nl.edu>
2025-02-28 01:01:43 -0600

    Create MulitplyList.py

---
+++ MulitplyList.py
@@ -0,0 +1,10 @@
+def multiply_list(numbers):
+    product = 1
+    for number in numbers:
+        product *= number
+    return product
+
+numbers = [5, 2, 7, -1]
+result = multiply_list(numbers)
+print(result)
+-70
