aa392121f0d1190395d8eadea9870e9e08fb6d10
zuvega <zvega@my.nl.edu>
2025-02-28 00:29:01 -0600

    Create AreaOfCircle.py

---
+++ AreaOfCircle.py
@@ -0,0 +1,11 @@
+import math
+
+def areaOfCircle(r):
+    area = math.pi * (r ** 2)
+    return area
+
+#example
+radius = 4
+results = areaOfCircle(radius)
+print(f"The area of the circle with radius {radius} is: {results}")
+The area of the circle with radius 4 is: 50.26548245743669
